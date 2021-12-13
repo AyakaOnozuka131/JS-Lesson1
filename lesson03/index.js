4a5be688abd5279686fea5fd1a8af337c736d397
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-12-13 22:38:32 +0900

    Feature: lesson03

---
+++ lesson03/index.js
@@ -0,0 +1,18 @@
+/** @type {Array} 画像パスの配列 */
+const imageUrl = ['bookmark.png', 'message.png'];
+const ul = document.getElementById('js-list');
+const fragment = document.createDocumentFragment();
+
+for (let i = 0; i < imageUrl.length; i++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+
+    a.href = `a${i+1}.html`;
+    a.textContent = `a${i+1}`;
+    img.src = `/img/${imageUrl[i]}`;
+
+    fragment.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
+}
+
+ul.appendChild(fragment);
