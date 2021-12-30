a8591103c7fa5b77d4605a09991b42ff0da0b9b6
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-12-30 19:51:55 +0900

    feat：lesson08

---
+++ lesson08/index.js
@@ -0,0 +1,66 @@
+const imageArray = [
+    {
+        img: "1.png", 
+        alt:"画像1", 
+        text: "ブックマーク"
+    }, 
+    {
+        img: "2.png", 
+        alt:"画像2", 
+        text: "メッセージ"
+    }
+];
+
+const ul = document.getElementById('js-list');
+
+const createLoading = () => {
+    const li = document.createElement("li");
+    const loadingImg = document.createElement('img');
+
+    li.style.listStyle = "none";
+    li.id = "js-loading";
+    loadingImg.src = 'loading-circle.gif';
+    ul.appendChild(li).appendChild(loadingImg);
+};
+
+const removeLoading = () => {
+    const li = document.getElementById('js-loading');
+
+    ul.removeChild(li);
+};
+
+const addList = (val) => {
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < val.length; i++) {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+    
+        li.textContent = val[i].text;
+        img.src = val[i].img;
+        img.alt = val[i].alt;
+    
+        fragment.appendChild(li).appendChild(img);
+    }
+
+    ul.appendChild(fragment);
+};
+
+/**
+ * Promiseを実行し、rejectの値を返す関数
+ * @return {promise object} 
+ */
+const fetchData = () => {
+    createLoading();
+    return new Promise( (reject) => {
+        setTimeout(() => {
+            reject(new Error("ERROR!"));
+        }, 3000);
+    });
+};
+
+/**
+ * fetchDataで返ってきたエラーをキャッチしてconsoleにエラーを出力する
+ */
+fetchData().catch((error) => {
+    console.error(error);
+});
