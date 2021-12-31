3431bfb19d2416eea42434d73fbf139a661864fc
AyakaOnozuka131 <c_yu_again@icloud.com>
2022-01-01 01:26:50 +0900

    feat : Lesson09

---
+++ lesson09/index.js
@@ -0,0 +1,69 @@
+const imageArray = [
+    {
+        img: "1.png", 
+        alt:"画像1", 
+        text: "ブックマーク"
+    }, 
+    {
+        img: "2.png", 
+        alt:"画像2", 
+        text: "メッセージ"
+    }
+];
+
+const ul = document.getElementById('js-list');
+
+const createLoading = () => {
+    const li = document.createElement("li");
+    const loadingImg = document.createElement('img');
+
+    li.style.listStyle = "none";
+    li.id = "js-loading";
+    loadingImg.src = 'loading-circle.gif';
+    ul.appendChild(li).appendChild(loadingImg);
+};
+
+const removeLoading = () => {
+    document.getElementById('js-loading').remove();
+};
+
+const addList = (val) => {
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < val.length; i++) {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+    
+        li.textContent = val[i].text;
+        img.src = val[i].img;
+        img.alt = val[i].alt;
+    
+        fragment.appendChild(li).appendChild(img);
+    }
+
+    ul.appendChild(fragment);
+};
+
+/**
+ * Promiseを実行し、resolveの値を返す関数
+ * @return {promise object} 
+ */
+const fetchData = () => {
+    createLoading();
+    return new Promise( resolve => {
+        setTimeout(() => {
+            resolve(imageArray);
+        }, 3000);
+    });
+};
+
+/**
+ * Async関数
+ * fetchDataで値が返ってきた後、addList関数にその値を渡す
+ */
+const init = async () => {
+    const data = await fetchData();
+    removeLoading();
+    addList(data);
+};
+
+init();
