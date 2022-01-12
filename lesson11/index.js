e3553496f0d85541b470bb329fd2d06f9488f5f0
AyakaOnozuka131 <c_yu_again@icloud.com>
2022-01-12 20:30:42 +0900

    Display error messages in fetchListData function.

--- lesson11/index.js
+++ lesson11/index.js
@@ -31,7 +31,7 @@ const fetchData = async () => {
         return json.data;
 
     } catch(error) {
-        console.error(error);
+        throw new Error(error);
     } 
 };
 
@@ -44,7 +44,7 @@ const fetchListData = async () => {
     try {
         const data = await fetchData();
         if( data.length === 0 ) {
-            throw new Error('データが空です。');
+            console.error(error);
         }
         return data;
 
